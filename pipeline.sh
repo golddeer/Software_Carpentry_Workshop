bd2d4fe401018a1a3e585312b89731cce701c850
Dior Kelley <diorrkelley@gmail.com>
2014-10-21 11:20:46 -0700

    added bash script

---
+++ pipeline.sh
@@ -0,0 +1,22 @@
+echo Cleaning up
+rm *.txt
+rm *.tmp
+rm *.csv
+
+echo Download data
+curl -L https://github.com/zonca/swcarpentry-workshop-pandas/blob/master/rawdata/rawdata.zip?raw=true -o rawdata.zip
+
+echo unpack data
+unzip rawdata.zip
+rm rawdata.zip
+
+rm *tmp
+
+for f in *.txt
+do
+	mv $f ${f/txt/csv}
+done
+
+echo Available files
+ls *.csv
+
